27360691fad4021da5249b5f52e110ab573c616e
seniorkot <seniorkot24@yandex.ru>
2017-10-28 02:08:25 +0300

    Entities created

---
+++ src/main/java/com/seniorkot/data/Point.java
@@ -0,0 +1,79 @@
+package com.seniorkot.data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Point{
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Double x;
+    private Double y;
+    private Double r;
+    private Boolean inside;
+
+    public Point(){}
+
+    public Point(Double x, Double y, Double r){
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.inside = check();
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getX() {
+        return x;
+    }
+    public void setX(Double x) {
+        this.x = x;
+    }
+
+    public Double getY() {
+        return y;
+    }
+    public void setY(Double y) {
+        this.y = y;
+    }
+
+    public Double getR() {
+        return r;
+    }
+    public void setR(Double r) {
+        this.r = r;
+    }
+
+    public Boolean getInside() {
+        return inside;
+    }
+    public void setInside(Boolean inside) {
+        this.inside = inside;
+    }
+
+    public boolean check(){
+        if (x <= 0 && y <= 0 && x >= -r && y >= -r/2){
+            return true;
+        }
+        else if (x <= 0 && y > 0 && x*x + y*y >= r/4){
+            return true;
+        }
+        else if (x >= 0 && y <= 0 && y >= x*2 - r){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return "Point [ x: " + this.x + ", y: " + this.y + ", R: " + this.r + ", inside: " + this.inside + " ]";
+    }
+}
