f6dc7be53b02131aa6f619fbbb8dd8cbaff0c087
seniorkot <seniorkot24@yandex.ru>
2017-10-27 23:11:53 +0300

    Creating Spring-boot application

---
+++ src/main/java/com/seniorkot/Lab9App.java
@@ -0,0 +1,11 @@
+package com.seniorkot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab9App {
+    public static void main(String[] args) {
+        SpringApplication.run(Lab9App.class, args);
+    }
+}
